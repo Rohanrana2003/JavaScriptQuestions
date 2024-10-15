9594f8639888298d309beffa90f96ed412b240a0
Rohanrana2003 <rohanrana33042@gmail.com>
2024-10-15 19:00:18 +0530

    Added 4 questions - 30 question javaScript

---
+++ 30 Short JavaScript Questions/script.js
@@ -0,0 +1,26 @@
+    // 1. Given a String Reverse each word in a sentence 
+    
+            // let name = 'Rohan bhai kese ho';
+            // let arr = name.split([' ']).map(element => {
+            // return element.split(['']).reverse().join('');
+            // }).join(' ')
+            // console.log(arr)
+
+    // 2. How to check an object is array or not 
+
+            // let obj = []
+            // Array.isArray(obj)? console.log('true'): console.log('false');
+
+    // 3. How to empty an array without reseting it and running any loop
+
+            // let arr = [1, 2, 3, 4];
+            // arr.length = 0;
+            // console.log(arr)
+
+    // 4. How would you check a number is an Integer
+
+            // function checkNum(num){
+            //     return Number.isInteger(num);
+            // }
+            // console.log(checkNum(17));
+
