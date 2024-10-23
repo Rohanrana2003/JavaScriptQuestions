5529c0298521eb8d7438497b0696033158e1363a
Rohanrana2003 <rohanrana33042@gmail.com>
2024-10-23 16:01:59 +0530

    Final Commit - Map Filter Reduce

--- let var const/script.js
+++ let var const/script.js
@@ -1,1 +1,32 @@
-console.log('first')
+
+//Shadowing
+    // function x() {
+
+    //     let a = 10;
+    //     let b = 20;
+
+    //     if (true) {
+    //         var a = 30; // give error because of illegal shadowing
+    //         let b = 40; //Legal shadowing
+    //         console.log(a);
+    //         console.log(b);
+    //     }
+
+    // }
+    // x()
+
+// Declaration without initialization
+    // var c;
+    // let a;
+    // const b; // Give error
+
+// Re - Initialization 
+    // var a = 10;
+    // a = 20;
+
+    // let b = 10;
+    // b = 20;
+
+    // const c = 10;
+    // c = 20; //Give an error
+
